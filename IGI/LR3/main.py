d518ec3a3e80bc64d3e54526287d82af87d62bd6
Konstantin <konstamah@gmail.com>
2024-03-27 16:49:40 +0300

    Menu in main.py

--- IGI/LR3/main.py
+++ IGI/LR3/main.py
@@ -1,7 +1,11 @@
-import Task1
-import Task2
-import Task3
-import Task4
-import Task5
+import Task1, Task2, Task3, Task4, Task5
+import repeat, input_check
 
-Task5.task()
+rep = True
+tasks = [Task1.task, Task2.task, Task3.task, Task4.task, Task5.task]
+while rep:
+    task = input_check.int_check("Выберите выполняемую задачу: \n1. Задание 1\n2. Задание 2\n3. Задание 3\n4. Задание 4\n5. Задание 5\n6. Завершить программу\n", 1, 6)
+    if task == 6:
+        rep = False
+    else:
+        tasks[task - 1]()
